7d86c1770813a4bf9afeae36d12090edf5c06972
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-08-29 23:24:56 -0600

    add styling to current uv index based on value

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,16 @@
 let cityName;
 let stateName;
 let zipCode;
-let localStorageArray = [];
+let searchHistory = [];
 let currentLat;
 let currentLon;
+let now = moment();
 
 apiKey = '3677b7f1ed1a37d22f29ccb0277f664a';
+
+displayDate();
+retrieveFromLocalStorage();
+addToSearchHistory(searchHistory);
 
 //function to return an ajax call from the current weather api based on a city input
 function getCurrentWeatherData(city) {
@@ -25,7 +30,7 @@ function getCurrentWeatherData(city) {
         $('#currentCity').text('City: ' + cityName);
         $('#currentIcon').attr('src', 'http://openweathermap.org/img/wn/' + response.weather[0].icon + '@2x.png');
         sendToLocalStorage(cityName, zipCode);
-        addToSearchHistory(localStorageArray);
+        addToSearchHistory(searchHistory);
     });
 }
 
@@ -38,11 +43,32 @@ function getFiveDayForecast(lat, long) {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
+        let uvi = response.current.uvi;
         console.log(response);
         $('#currentTemp').text('Temperature: ' + response.current.temp + (' ') + String.fromCharCode(176) + 'F');
         $('#currentHumidity').text('Humidity: ' + response.current.humidity + '%');
         $('#currentWind').text('Wind Speed: ' + response.current.wind_speed + ' MPH');
-        $('#currentUV').text('UV Index: ' + response.current.uvi);
+        $('#currentUV').addClass('border rounded p-1').text(response.current.uvi);
+        if (uvi < 3) {
+            console.log('green');
+            $('#currentUV').addClass('bg-success');
+        }
+        else if (uvi >= 3 & uvi < 6) {
+            console.log('yellow');
+            $('#currentUV').addClass('bg-warning');
+        }
+        else if (uvi >= 6 & uvi < 8) {
+            console.log('orange');
+            $('#currentUV').css('background-color', '#FF5E13');
+        }
+        else if (uvi >= 8 & uvi < 11) {
+            console.log('red');
+            $('#currentUV').addClass('bg-danger');
+        }
+        else if (uvi >= 11) {
+            console.log('purple');
+            $('#currentUV').css('background-color', '#800080');
+        }
         displayFiveDayForecast(response);
     });
 }
@@ -52,25 +78,25 @@ function getFiveDayForecast(lat, long) {
 //function to store form input to local storage
 function sendToLocalStorage(city, zip) {
     let citySearch = { city: city, zip: zip };
-    localStorageArray.unshift(citySearch);
+    searchHistory.unshift(citySearch);
     console.log('hit');
-    console.log(localStorageArray);
-    if(localStorageArray.length > 6) {
-        localStorageArray.pop();
+    console.log(searchHistory);
+    if (searchHistory.length > 6) {
+        searchHistory.pop();
     }
-    // localStorage.setItem('cityName', city);
-    // localStorage.setItem('zipCode', zip);
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
 }
 
 function retrieveFromLocalStorage() {
-    localStorage.getItem('cityName');
+    searchHistory = JSON.parse(localStorage.getItem('searchHistory'));
+    console.log(searchHistory);
 }
 
 // function to store form input to search history; display search history
-function addToSearchHistory(localStorageArray) {
-    for (let i = 0; i < localStorageArray.length; i++) {
-        $('#history' + i).text(localStorageArray[i].city);
-        console.log(localStorageArray[i].city);
+function addToSearchHistory(searchHistory) {
+    for (let i = 0; i < searchHistory.length; i++) {
+        $('#history' + i).text(searchHistory[i].city);
+        console.log(searchHistory[i].city);
         $('#history' + i).removeClass('collapse');
         $('#history' + i).addClass('collapse.show');
     }
@@ -78,12 +104,16 @@ function addToSearchHistory(localStorageArray) {
 
 function displayFiveDayForecast(responseFiveDay) {
     for (let i = 1; i < 6; i++) {
-        $('#date' + i).text('Date' + i);
+        $('#date' + i).text(now.add(1, 'days').format('l'));
         $('#icon' + i).attr('src', 'http://openweathermap.org/img/wn/' + responseFiveDay.daily[i].weather[0].icon + '@2x.png');
         $('#minTemp' + i).text('Min: ' + responseFiveDay.daily[i].temp.min + (' ') + String.fromCharCode(176) + 'F');
         $('#maxTemp' + i).text('Max: ' + responseFiveDay.daily[i].temp.max + (' ') + String.fromCharCode(176) + 'F');
         $('#humid' + i).text('Humidity: ' + responseFiveDay.daily[i].humidity + '%');
     };
+}
+
+function displayDate() {
+    $('#header').text('FIVE DAY FORECAST - ' + now.format('l'));
 }
 
 // click listener on submit button
