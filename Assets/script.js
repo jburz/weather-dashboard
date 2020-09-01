5aeb19d14e50dd059e5fdef76b5b45f8e87bb29d
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-08-31 18:59:26 -0600

    search history buttons now return a new ajax call, fixed bug where uvi was not triggering correct color change

--- Assets/script.js
+++ Assets/script.js
@@ -4,18 +4,18 @@ let zipCode;
 let searchHistory = [];
 let currentLat;
 let currentLon;
-let now = moment();
+// let now = moment();
 
 apiKey = '3677b7f1ed1a37d22f29ccb0277f664a';
 
 displayDate();
 retrieveFromLocalStorage();
-if(searchHistory !== null) {
+if (searchHistory !== null) {
     addToSearchHistory(searchHistory);
 }
 
 //function to return an ajax call from the current weather api based on a city input
-function getCurrentWeatherData(city) {
+function getCurrentWeatherData(zipCode) {
     //variable for URL to api request
     const queryURL = 'https://api.openweathermap.org/data/2.5/weather?zip=' + zipCode + '&units=imperial&appid=' + apiKey;
 
@@ -48,26 +48,37 @@ function getFiveDayForecast(lat, long) {
         $('#currentTemp').text('Temperature: ' + response.current.temp + (' ') + String.fromCharCode(176) + 'F');
         $('#currentHumidity').text('Humidity: ' + response.current.humidity + '%');
         $('#currentWind').text('Wind Speed: ' + response.current.wind_speed + ' MPH');
-        $('#currentUV').addClass('border rounded p-1').text(response.current.uvi);
+        $('#currentUV').addClass('border rounded p-1').text(uvi);
         if (uvi < 3) {
             $('#currentUV').addClass('bg-success');
+            $('#currentUV').removeClass('bg-warning');
+            $('#currentUV').removeClass('bg-danger');
         }
         else if (uvi >= 3 & uvi < 6) {
             $('#currentUV').addClass('bg-warning');
+            $('#currentUV').removeClass('bg-success');
+            $('#currentUV').removeClass('bg-danger');
         }
         else if (uvi >= 6 & uvi < 8) {
             $('#currentUV').css('background-color', '#FF5E13');
+            $('#currentUV').removeClass('bg-success');
+            $('#currentUV').removeClass('bg-warning');
+            $('#currentUV').removeClass('bg-danger');
         }
         else if (uvi >= 8 & uvi < 11) {
             $('#currentUV').addClass('bg-danger');
+            $('#currentUV').removeClass('bg-success');
+            $('#currentUV').removeClass('bg-warning');
         }
         else if (uvi >= 11) {
             $('#currentUV').css('background-color', '#800080');
+            $('#currentUV').removeClass('bg-success');
+            $('#currentUV').removeClass('bg-warning');
+            $('#currentUV').removeClass('bg-danger');
         }
         displayFiveDayForecast(response);
     });
 }
-
 
 //function to store form input to local storage
 function sendToLocalStorage(city, zip) {
@@ -91,13 +102,13 @@ function addToSearchHistory(searchHistory) {
         $('#history' + i).text(searchHistory[i].city);
         $('#history' + i).removeClass('collapse');
         $('#history' + i).addClass('collapse.show');
-        $('#history' + i).data('zipCode', zipCode);
+        $('#history' + i).data('zipCode', searchHistory[i].zip);
     }
 }
 
 function displayFiveDayForecast(responseFiveDay) {
     for (let i = 1; i < 6; i++) {
-        $('#date' + i).text(now.add(1, 'days').format('l'));
+        $('#date' + i).text(moment().add(i, 'days').format('l'));
         $('#icon' + i).attr('src', 'http://openweathermap.org/img/wn/' + responseFiveDay.daily[i].weather[0].icon + '@2x.png');
         $('#minTemp' + i).text('Min: ' + responseFiveDay.daily[i].temp.min + (' ') + String.fromCharCode(176) + 'F');
         $('#maxTemp' + i).text('Max: ' + responseFiveDay.daily[i].temp.max + (' ') + String.fromCharCode(176) + 'F');
@@ -106,7 +117,7 @@ function displayFiveDayForecast(responseFiveDay) {
 }
 
 function displayDate() {
-    $('#header').text('FIVE DAY FORECAST - ' + now.format('l'));
+    $('#header').text('FIVE DAY FORECAST - ' + moment().format('l'));
 }
 
 // click listener on submit button
@@ -128,12 +139,11 @@ $('#submitBtn').on('click', function (event) {
         $('#stateName').val('');
         $('#zipCode').val('');
         // run getCurrentWeatherData using cityName
-        getCurrentWeatherData(cityName);
+        getCurrentWeatherData(zipCode);
     }
 });
 
 //click listener on each city search button
-$('.list-group').on('click', function(event) {
-    console.log(event);
-    console.dir(event.target);
+$('.list-group').on('click', function (event) {
+    getCurrentWeatherData($(event.target).data().zipCode);
 });
